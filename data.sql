edaf8a515b32c46411bdd759dc038f3e2ba6e51c
Tameusz <szustakmateusz9@gmail.com>
2021-04-30 21:21:33 +0200

    Connect with database
    Co-Authored-By: piotrkgithub <73697473+piotrkgithub@users.noreply.github.com>

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
 create table PRODUCT
 (
     ID       INT,
-    BRAND    VARCHAR2,
-    NAME     VARCHAR2,
+    BRAND    VARCHAR2(20),
+    NAME     VARCHAR2(50),
     PRICE    FLOAT,
     QUANTITY INT
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/product.csv');
 
 create table COOLER
 (
@@ -19,7 +19,7 @@ create table COOLER
         primary key (ID),
     constraint COOLER_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/cooler.csv');
 
 create table CPU
 (
@@ -38,15 +38,15 @@ create table CPU
         primary key (ID),
     constraint CPU_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/cpu.csv');
 
 create table MOTHERBOARD
 (
     ID            INT auto_increment,
-    TIER          INT,
-    CHIPSET       VARCHAR2,
-    SOCKET        VARCHAR2,
-    FORM_FACTOR   VARCHAR2,
+    TIER          FLOAT,
+    CHIPSET       VARCHAR2(10),
+    SOCKET        VARCHAR2(10),
+    FORM_FACTOR   VARCHAR2(10),
     MEMORY_SLOTS  INT,
     MEMORY_MAX_GB INT,
     PRODUCT_FK    INT,
@@ -54,7 +54,7 @@ create table MOTHERBOARD
         primary key (ID),
     constraint MOTHERBOARD_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/motherboard.csv');
 
 create table RAM
 (
@@ -69,31 +69,31 @@ create table RAM
         primary key (ID),
     constraint RAM_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/ram.csv');
 
 create table STORAGE
 (
     ID          INT auto_increment,
     CAPACITY_GB INT,
     TIER        INT,
-    TYPE        VARCHAR2,
-    FORM_FACTOR VARCHAR2,
-    INTERFACE   VARCHAR2,
+    TYPE        VARCHAR2(10),
+    FORM_FACTOR VARCHAR2(10),
+    INTERFACE   VARCHAR2(20),
     PRODUCT_FK  INT,
     constraint STORAGE_PK
         primary key (ID),
     constraint STORAGE_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/storage.csv');
 
 create table GPU
 (
     ID                    INT auto_increment,
-    CHIPSET               VARCHAR2,
+    CHIPSET               VARCHAR2(30),
     MEMORY_GB             INT,
     CORE_CLOCK_MHZ        INT,
     BOOST_CLOCK_MHZ       INT,
-    LENGHT_MM             INT,
+    LENGHT_MM             FLOAT,
     TDP                   INT,
     RECOMMENDED_PSU_WATTS INT,
     PERFORMANCE           INT,
@@ -102,46 +102,46 @@ create table GPU
         primary key (ID),
     constraint GPU_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/gpu.csv');
 
 create table CASE_
 (
     ID                    INT auto_increment,
-    POWER_SUPPLY_STANDARD VARCHAR2,
-    MAX_MOTHERBOARD_SIZE  VARCHAR2,
-    TYPE                  VARCHAR2,
-    SIDE_PANEL_WINDOW     VARCHAR2,
+    POWER_SUPPLY_STANDARD VARCHAR2(5),
+    MAX_MOTHERBOARD_SIZE  VARCHAR2(10),
+    TYPE                  VARCHAR2(20),
+    SIDE_PANEL_WINDOW     VARCHAR2(30),
     PRODUCT_FK            INT,
     constraint CASE__PK
         primary key (ID),
     constraint CASE__PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/case.csv');
 
 create table PSU
 (
     ID                INT auto_increment,
     TIER              FLOAT,
-    FORM_FACTOR       VARCHAR2,
-    EFFICIENCY_RATING VARCHAR2,
+    FORM_FACTOR       VARCHAR2(5),
+    EFFICIENCY_RATING VARCHAR2(20),
     WATTAGE           INT,
-    MODULAR           VARCHAR2,
+    MODULAR           VARCHAR2(10),
     PRODUCT_FK        INT,
     constraint PSU_PK
         primary key (ID),
     constraint PSU_PRODUCT_ID_FK
         foreign key (PRODUCT_FK) references PRODUCT (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/psu.csv');
 
 create table USER
 (
     ID       INT auto_increment,
-    EMAIL    VARCHAR2,
-    USERNAME VARCHAR2,
-    PASSWORD VARCHAR2,
+    EMAIL    VARCHAR2(100),
+    USERNAME VARCHAR2(50),
+    PASSWORD VARCHAR2(50),
     constraint USER_PK
         primary key (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/user.csv');
 
 create table "order"
 (
@@ -152,7 +152,7 @@ create table "order"
         primary key (ID),
     constraint ORDER_USER_ID_FK
         foreign key (USER_ID) references USER (ID)
-);
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/order.csv');
 
 create table ORDERLIST
 (
@@ -167,5 +167,4 @@ create table ORDERLIST
         foreign key (ORDER_ID) references "order" (ID),
     constraint ORDERLIST_PRODUCT_ID_FK
         foreign key (PRODUCT_ID) references PRODUCT (ID)
-);
-
+)AS SELECT * FROM CSVREAD('C:/Users/szust/Desktop/csv/orderlist.csv');
