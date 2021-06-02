f66a662cc990767fc02f40c75f5681222ff5b9c7
michawiet <michawiet@gmail.com>
2021-06-02 22:54:04 +0200

    Workaround for google double click sign-in
    Co-Authored-By: Batsza <73697255+Batsza@users.noreply.github.com>

--- front/src/components/AuthenticationForms/SignIn.js
+++ front/src/components/AuthenticationForms/SignIn.js
@@ -1,27 +1,13 @@
-import React, {useRef, useState}from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import React, { useRef, useState, useEffect } from 'react';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Grid, Box,
+  Typography, makeStyles, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import { useHistory } from "react-router-dom";
 import GoogleButton from 'react-google-button'
-import PropTypes from 'prop-types';
 import { useAuth } from "../../AuthContext"
 import Alert from '@material-ui/lab/Alert';
 import firebase from "firebase/app";
 import "firebase/auth";
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -43,37 +29,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function SignIn() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const classes = useStyles();
   let history = useHistory();
+  const [loginSuccess, setLoginSuccess] = useState(false);
 
-
-
+  useEffect(() => {
+    if(loginSuccess) {
+      history.push("/loged");
+    }
+  }, [loginSuccess]);
 
   async function handleSubmit(e) {
-    e.preventDefault()
-
+    e.preventDefault();
     try {
-
-      setError("")
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push("/loged")
-
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      setLoginSuccess(true);
     } catch {
-      setError("Niepoprawne hasło lub e-mail")
-
+      setError("Niepoprawne hasło lub e-mail");
     }
-
-    setLoading(false)
+    setLoading(false);
   }
- 
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -81,14 +65,11 @@ export default function SignIn() {
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        
-
         <Typography component="h1" variant="h5">
           Zaloguj
         </Typography>
         <form className={classes.form} noValidate>
           <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Adres Email" name="email" autoComplete="email" autoFocus inputRef={emailRef}/>
-
           <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Hasło" type="password" id="password" autoComplete="current-password" inputRef={passwordRef}/>
           <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Zapamiętaj mnie"/>
           <Button disabled={loading}  fullWidth variant="contained" color="primary"className={classes.submit} onClick={handleSubmit}>
@@ -97,9 +78,9 @@ export default function SignIn() {
           {error && <Alert severity="error">{error}</Alert>}
           <GoogleButton variant="contained" color="primary"className={classes.submit}    
           onClick={() => {
-                const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+              var googleAuthProvider = new firebase.auth.GoogleAuthProvider();
               firebase.auth().signInWithPopup(googleAuthProvider).then(function() {
-                history.push("/loged");
+                setLoginSuccess(true);
               }).catch(function(error) {
                 console.log(error);
               });
