ba4b20aa6c86f5cbd05d858cd41de5cbe5a95ddb
Tameusz <szustakmateusz9@gmail.com>
2021-06-05 20:04:52 +0200

    Update ComputerController.java
    Co-Authored-By: piotrkgithub <73697473+piotrkgithub@users.noreply.github.com>

--- backend/src/main/java/com/pcc/pc_configurator/controller/ComputerController.java
+++ backend/src/main/java/com/pcc/pc_configurator/controller/ComputerController.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 @RestController
@@ -47,6 +48,56 @@ public class ComputerController {
     List<PsuDTO> psuDtoList = new ArrayList<>();
     List<RamDTO> ramDtoList = new ArrayList<>();
     List<StorageDTO> storageDtoList = new ArrayList<>();
+    Float[][] wspOffice = {
+            {0.0f, 0.5f},   //cpu
+            {0.0f, 0.5f},   //cooler
+            {0.0f, 0.5f},   //motherboard
+            {0.0f, 0.5f},   //ram
+            {0.0f, 0.5f},   //storage
+            {0.0f, 0.5f},   //gpu
+            {0.0f, 0.5f},   //psu
+            {0.0f, 0.5f}    //case
+    };
+    Float[][] wspGames = {
+            {0.0f, 0.5f},   //cpu
+            {0.0f, 0.5f},   //cooler
+            {0.0f, 0.5f},   //motherboard
+            {0.0f, 0.5f},   //ram
+            {0.0f, 0.5f},   //storage
+            {0.0f, 0.5f},   //gpu
+            {0.0f, 0.5f},   //psu
+            {0.0f, 0.5f}    //case
+    };
+    Float[][] wspPhotoEditing = {
+            {0.0f, 0.5f},   //cpu
+            {0.0f, 0.5f},   //cooler
+            {0.0f, 0.5f},   //motherboard
+            {0.0f, 0.5f},   //ram
+            {0.0f, 0.5f},   //storage
+            {0.0f, 0.5f},   //gpu
+            {0.0f, 0.5f},   //psu
+            {0.0f, 0.5f}    //case
+    };
+    Float[][] wspVideoEditing = {
+            {0.0f, 0.5f},   //cpu
+            {0.0f, 0.5f},   //cooler
+            {0.0f, 0.5f},   //motherboard
+            {0.0f, 0.5f},   //ram
+            {0.0f, 0.5f},   //storage
+            {0.0f, 0.5f},   //gpu
+            {0.0f, 0.5f},   //psu
+            {0.0f, 0.5f}    //case
+    };
+    Float[][] wsp3DRendering = {
+            {0.0f, 0.5f},   //cpu
+            {0.0f, 0.5f},   //cooler
+            {0.0f, 0.5f},   //motherboard
+            {0.0f, 0.5f},   //ram
+            {0.0f, 0.5f},   //storage
+            {0.0f, 0.5f},   //gpu
+            {0.0f, 0.5f},   //psu
+            {0.0f, 0.5f}    //case
+    };
 
     @EventListener(ApplicationReadyEvent.class)
     //@RequestMapping
@@ -73,49 +124,58 @@ public class ComputerController {
         List<GpuDTO> subGpuList = new ArrayList<>();
         List<PsuDTO> subPsuList = new ArrayList<>();
         List<CaseDTO> subCaseList = new ArrayList<>();
-        Random random = new Random();
 
-        if (type.equals("office")) {
+        Map<String, Float[][]> map = new HashMap<>() {{
+            put("office", wspOffice);
+            put("games", wspGames);
+            put("photoEditing", wspPhotoEditing);
+            put("videoEditing", wspVideoEditing);
+            put("3DRendering", wsp3DRendering);
+        }};
+        Random random = new Random();
+        if (map.containsKey(type)) {
+            Float[][] temporaryWsp = map.get(type);
+
             for (int i = 0; i < cpuDtoList.size(); ++i) {
-                if (cpuDtoList.get(i).getProduct().getPrice() >= price * 0.0 && cpuDtoList.get(i).getProduct().getPrice() <= price * 0.22 && cpuDtoList.get(i).getProduct().getBrand().equals(cpu)) {
+                if (cpuDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[0][0] && cpuDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[0][1] && cpuDtoList.get(i).getProduct().getBrand().equals(cpu)) {
                     subCpuList.add(cpuDtoList.get(i));
                 }
             }
             for (int i = 0; i < coolerDtoList.size(); ++i) {
-                if (coolerDtoList.get(i).getProduct().getPrice() >= price * 0.0 && coolerDtoList.get(i).getProduct().getPrice() <= price * 0.5) {
+                if (coolerDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[1][0] && coolerDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[1][1]) {
                     subCoolerList.add(coolerDtoList.get(i));
                 }
             }
             products.putIfAbsent("cpu",subCpuList.get(random.nextInt(subCpuList.size()-1)));
             for (int i = 0; i < motherboardDtoList.size(); ++i) {
-                if (motherboardDtoList.get(i).getProduct().getPrice() >= price * 0.0 && motherboardDtoList.get(i).getProduct().getPrice() <= price * 0.5 && motherboardDtoList.get(i).getSocket().equals(((CpuDTO) products.get("cpu")).getSocket())) {
+                if (motherboardDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[2][0] && motherboardDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[2][1]&& motherboardDtoList.get(i).getSocket().equals(((CpuDTO) products.get("cpu")).getSocket())) {
                     subMotherboardList.add(motherboardDtoList.get(i));
                 }
             }
             for (int i = 0; i < ramDtoList.size(); ++i) {
-                if (ramDtoList.get(i).getProduct().getPrice() >= price * 0.0 && ramDtoList.get(i).getProduct().getPrice() <= price * 0.5 ) {
+                if (ramDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[3][0] && ramDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[3][1] ) {
                     subRamList.add(ramDtoList.get(i));
                 }
             }
             for (int i = 0; i < storageDtoList.size(); ++i) {
-                if (storageDtoList.get(i).getProduct().getPrice() >= price * 0.0 && storageDtoList.get(i).getProduct().getPrice() <= price * 0.5 ) {
+                if (storageDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[4][0] && storageDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[4][1] ) {
                     subStorageList.add(storageDtoList.get(i));
                 }
             }
             for (int i = 0; i < gpuDtoList.size(); ++i) {
-                if (gpuDtoList.get(i).getProduct().getPrice() >= price * 0.0 && gpuDtoList.get(i).getProduct().getPrice() <= price * 0.5 && gpuDtoList.get(i).getChipset().substring(0,gpuDtoList.get(i).getChipset().indexOf(' ')).equals(gpu)// &&
+                if (gpuDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[5][0] && gpuDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[5][1] && gpuDtoList.get(i).getChipset().substring(0,gpuDtoList.get(i).getChipset().indexOf(' ')).equals(gpu)// &&
                     /*!((CpuDTO) products.get("cpu")).isIntegratedGPU()*/) {
                     subGpuList.add(gpuDtoList.get(i));
                 }
             }
             for (int i = 0; i < psuDtoList.size(); ++i) {
-                if (psuDtoList.get(i).getProduct().getPrice() >= price * 0.0 && psuDtoList.get(i).getProduct().getPrice() <= price * 0.5 &&
+                if (psuDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[6][0] && psuDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[6][1] &&
                         (((CpuDTO) products.get("cpu")).getTdpW()) + 100 <= psuDtoList.get(i).getWattage()) {
                     subPsuList.add(psuDtoList.get(i));
                 }
             }
             for (int i = 0; i < caseDtoList.size(); ++i) {
-                if (caseDtoList.get(i).getProduct().getPrice() >= price * 0.0 && caseDtoList.get(i).getProduct().getPrice() <= price * 0.5 ) {
+                if (caseDtoList.get(i).getProduct().getPrice() >= price * temporaryWsp[7][0] && caseDtoList.get(i).getProduct().getPrice() <= price * temporaryWsp[7][1] ) {
                     subCaseList.add(caseDtoList.get(i));
                 }
             }
@@ -127,11 +187,11 @@ public class ComputerController {
             products.putIfAbsent("psu",subPsuList.get(random.nextInt(subPsuList.size()-1)));
             products.putIfAbsent("case",subCaseList.get(random.nextInt(subCaseList.size()-1)));
         }
-        var temp = ((CpuDTO) products.get("cpu")).getProduct().getPrice() +((CoolerDTO) products.get("cooler")).getProduct().getPrice() +
+        var temp1 = ((CpuDTO) products.get("cpu")).getProduct().getPrice() +((CoolerDTO) products.get("cooler")).getProduct().getPrice() +
                 ((MotherboardDTO) products.get("motherboard")).getProduct().getPrice() +((RamDTO) products.get("ram")).getProduct().getPrice() +
                 ((StorageDTO) products.get("storage")).getProduct().getPrice() +((GpuDTO) products.get("gpu")).getProduct().getPrice() +
                 ((PsuDTO) products.get("psu")).getProduct().getPrice() +((CaseDTO) products.get("case")).getProduct().getPrice();
-        System.out.println(temp);
+        System.out.println(Math.round((temp1)*100.f)/100.f);
 
         return products;
     }
@@ -143,9 +203,9 @@ public class ComputerController {
                                                   @RequestParam("price") Float price) {
         List<Map<String, Object>> lists = new ArrayList<>();
 
+        lists.add(temp(type, cpu, gpu, price + price*0.6f));
         lists.add(temp(type, cpu, gpu, price));
-        lists.add(temp(type, cpu, gpu, price));
-        lists.add(temp(type, cpu, gpu, price));
+        lists.add(temp(type, cpu, gpu, price - price*0.6f));
         return lists;
     }
 }
