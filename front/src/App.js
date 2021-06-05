ea2d6d2d0fe9584722e964a7a7142e54a858ec0e
michawiet <michawiet@gmail.com>
2021-06-06 00:58:31 +0200

    Update App.js
    Co-Authored-By: Batsza <73697255+Batsza@users.noreply.github.com>
    Co-Authored-By: piotrkgithub <73697473+piotrkgithub@users.noreply.github.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,5 @@
-import UnauthenticatedPartPicker from './components/UnauthenticatedPartPicker';
-import SignIn from './components/AuthenticationForms/SignIn';
-import SingUp from './components/AuthenticationForms/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AuthenticatedPartPicker from './components/AuthenticatedPartPicker';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { AuthProvider } from "./AuthContext"
-import PrivateRoute from "./PrivateRoute"
-import Basket from './components/Cart/Cart'
 import routes from './components/Routes';
 import RouteWithSubRoutes from './components/RouteWithSubRoutes';
 
